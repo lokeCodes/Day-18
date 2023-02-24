1e60f19cd0ac762df0a21a2cbbfd1255284cf1b0
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-24 21:14:41 +0530

    Add files via upload

---
+++ SONGS.java
@@ -0,0 +1,22 @@
+// https://www.codechef.com/problems/SONGS
+// Question Link^^^
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int journey = s.nextInt();
+		    int songdur = s.nextInt();
+		    int timessongheard = journey/(3*songdur);
+		    System.out.println(timessongheard);
+		}
+	}
+}
